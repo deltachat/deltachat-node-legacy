3e4a5d07567458c344b8bc8f70ff1905e4503508
karissa <krmckelv@gmail.com>
2017-12-30 12:06:26 -0500

    add more functions

--- binding.cc
+++ binding.cc
@@ -145,6 +145,121 @@ NAN_METHOD(mrmailbox_unref) {
   mrmailbox_unref(mailbox->state);
 }
 
+NAN_METHOD(mrmailbox_get_chatlist) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], listflags);
+  v8::String::Utf8Value query(info[2]);
+  info.GetReturnValue().Set(
+    mrmailbox_get_chatlist(mailbox->state, listflags, *query)
+  );
+}
+
+NAN_METHOD(mrmailbox_get_chat) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  info.GetReturnValue().Set(
+    mrmailbox_get_chat(mailbox->state, chat_id)
+  )
+}
+
+NAN_METHOD(mrmailbox_marknoticed_chat) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  mrmailbox_marknoticed_chat(mailbox->state, chat_id)
+}
+
+NAN_METHOD(mrmailbox_get_chat_id_by_contact_id) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], contact_id);
+  info.GetReturnValue().Set(
+    Nan::New<v8::Number>(mrmailbox_get_chat_id_by_contact_id(mailbox->state, contact_id))
+  )
+}
+
+NAN_METHOD(mrmailbox_get_chat_media) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  ASSERT_UINT(info[2], msg_type);
+  ASSERT_UINT(info[3], or_msg_type);
+  info.GetReturnValue().Set(
+    mrmailbox_get_chat_media(mailbox->state, contact_id)
+  )
+}
+
+NAN_METHOD(mrmailbox_get_next_media) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], curr_msg_id);
+  ASSERT_UINT(info[2], dir);
+  info.GetReturnValue().Set(
+    Nan::New<v8::Number>(mrmailbox_get_next_media(mailbox->state, curr_msg_id, dir))
+  )
+}
+
+NAN_METHOD(mrmailbox_get_chat_contacts) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  info.GetReturnValue().Set(
+    mrmailbox_get_next_media(mailbox->state, chat_id)
+  )
+}
+
+NAN_METHOD(mrmailbox_get_fresh_msgs) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  info.GetReturnValue().Set(
+    mrmailbox_get_fresh_msgs(mailbox->state)
+  )
+}
+
+NAN_METHOD(mrmailbox_search_msgs) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  v8::String::Utf8Value query(info[2]);
+  info.GetReturnValue().Set(
+    mrmailbox_search_msgs(mailbox->state, chat_id, *query)
+  )
+}
+
+NAN_METHOD(mrmailbox_set_draft) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  v8::String::Utf8Value msg(info[2]);
+  info.GetReturnValue().Set(
+    mrmailbox_set_draft(mailbox->state, chat_id, *msg)
+  )
+}
+
+NAN_METHOD(mrmailbox_get_total_msg_count) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  info.GetReturnValue().Set(
+    Nan::New<v8::Number>(mrmailbox_get_total_msg_count(mailbox->state, chat_id))
+  )
+}
+
+NAN_METHOD(mrmailbox_get_fresh_msg_count) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  info.GetReturnValue().Set(
+    Nan::New<v8::Number>(mrmailbox_get_fresh_msg_count(mailbox->state, chat_id))
+  )
+}
+
+NAN_METHOD(mrmailbox_archive_chat) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  mrmailbox_archive_chat(mailbox->state, chat_id)
+}
+
+NAN_METHOD(mrmailbox_delete_chat) {
+  ASSERT_UNWRAP(info[0], mailbox, MrMailboxWrap);
+  ASSERT_UINT(info[1], chat_id);
+  mrmailbox_delete_chat(mailbox->state, chat_id)
+}
+
+
+
+
+
 /** 
  * mrarray
  **/
@@ -208,7 +323,21 @@ NAN_MODULE_INIT(InitAll) {
   EXPORT_FUNCTION(mrmailbox_create_chat_by_contact_id);
   EXPORT_FUNCTION(mrmailbox_send_text_msg);
   EXPORT_FUNCTION(mrmailbox_get_chat_msgs);
+  EXPORT_FUNCTION(mrmailbox_get_chatlist);
+  EXPORT_FUNCTION(mrmailbox_get_chat);
   EXPORT_FUNCTION(mrmailbox_get_msg);
+  EXPORT_FUNCTION(mrmailbox_marknoticed_chat);
+  EXPORT_FUNCTION(mrmailbox_get_chat_id_by_contact_id);
+  EXPORT_FUNCTION(mrmailbox_get_chat_media);
+  EXPORT_FUNCTION(mrmailbox_get_next_media);
+  EXPORT_FUNCTION(mrmailbox_get_chat_contacts);
+  EXPORT_FUNCTION(mrmailbox_get_fresh_msgs);
+  EXPORT_FUNCTION(mrmailbox_search_msgs);
+  EXPORT_FUNCTION(mrmailbox_set_draft);
+  EXPORT_FUNCTION(mrmailbox_get_total_msg_count);
+  EXPORT_FUNCTION(mrmailbox_get_fresh_msg_count);
+  EXPORT_FUNCTION(mrmailbox_archive_chat);
+  EXPORT_FUNCTION(mrmailbox_delete_chat);
 
   EXPORT_FUNCTION(mrarray_get_cnt);
   EXPORT_FUNCTION(mrarray_get_id);
