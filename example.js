a1c42be64c6a1d3358096a678a1e9a5b67d56c66
Karissa McKelvey <krmckelv@gmail.com>
2018-07-20 11:19:39 +0200

    new javascript API with example

--- example.js
+++ example.js
@@ -1,11 +1,9 @@
 var Pool = require('threads').Pool
-var deltachat = require('.')
+var Deltachat = require('.')
 var minimist = require('minimist')
 
 var argv = minimist(process.argv.slice(2))
 
-
-console.log('Delta.Chat Version ' + deltachat.dc_get_version_str())
 if (!argv.email || !argv.password) {
   console.error("--email and --password required")
   process.exit(1)
@@ -17,33 +15,30 @@ function cb (event, data1, data2) {
   return 0
 }
 
-var context = new deltachat.dc_context_new(cb)
+var deltachat = new Deltachat(cb)
 
-deltachat.dc_open(context)
+deltachat.open()
 
-deltachat.dc_set_config(context, 'addr', argv.email)
-deltachat.dc_set_config(context, 'mail_pw', argv.password)
+deltachat.set_config('addr', argv.email)
+deltachat.set_config('mail_pw', argv.password)
 
-deltachat.dc_configure(context)
+deltachat.configure()
 
-const deps = {
-  deltachat: '.'
-}
-
-var connected = deltachat.dc_is_configured(context)
+var connected = deltachat.is_configured()
 console.log('connected?', connected)
 
-var contact_id = deltachat.dc_create_contact(context, 'Karissa', "okdistribute@riseup.net")
-var contact = deltachat.dc_get_contact(context, contact_id)
-var info = deltachat.dc_contact_get_name_n_addr(contact)
+var contact_id = deltachat.create_contact('Karissa', "okdistribute@riseup.net")
+console.log(contact_id)
+var contact = deltachat.get_contact(contact_id)
+var info = deltachat.contact_get_name_n_addr(contact)
 console.log('contact', contact, info)
-var chat_id = deltachat.dc_create_chat_by_contact_id(context, contact_id);
+var chat_id = deltachat.create_chat_by_contact_id(contact_id)
 console.log('created chat with id:', chat_id)
-var msglist = deltachat.dc_get_chat_msgs(context, chat_id, 0, 0)
-console.log('has', deltachat.dc_array_get_cnt(msglist), 'chats')
+var msglist = deltachat.get_chat_msgs(chat_id, 0, 0)
+console.log('has', deltachat.array_get_cnt(msglist), 'chats')
 
 setTimeout(function () {
-  var connected = deltachat.dc_configure(context)
+  var connected = deltachat.configure()
   console.log('connected?', connected)
-  deltachat.dc_send_text_msg(context, chat_id, "Hi, here is my first message!");
+  deltachat.send_text_msg(chat_id, "Hi, here is my first message!");
 }, 30000)
